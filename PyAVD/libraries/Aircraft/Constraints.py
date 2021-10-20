67582e4a37e7770a436a4ac716ed18dc23469bfb
Gabisanth <gabisanth11@gmail.com>
2021-10-20 14:07:36 +0100

    Update Constraints.py

--- PyAVD/libraries/Aircraft/Constraints.py
+++ PyAVD/libraries/Aircraft/Constraints.py
@@ -5,15 +5,18 @@ class Constraints:
     ---> Handles Airworthiness regulations (FAR25 for now) and performance constraints defined in Aircraft.Spec()
     '''
     
-    def __init__(self,AR,e,LD_max,FieldLength,MaxVstall,Cl_max,Cl_clean):
+    def __init__(self,AR,e,LD_max,FieldLength,max_Vstall,Cl_max,Cl_clean):
         self.AR = AR
         self.e = e
         self.LD_max = LD_max
         self.FL = FieldLength
-        self.MaxVstall = MaxVstall
+        self.max_Vstall = max_Vstall
         self.Cl_max = Cl_max # this Cl_max is the same as Cl max for landing.
         self.Cl_clean = Cl_clean
+        self.Cd0 = 3.14 * AR * e /((2*LD_max)**2)
     
+    #FIELD PERFORMANCE CONSTRAINTS.
+
     # Using Roskam Method.
     def takeoff(self):
         TOP = self.FL/37.5 #gives TOP in lbs/(ft^2)
@@ -23,20 +26,40 @@ class Constraints:
 
     #Using Roskam Method.
     def landingRoskam(self,rho):
-        V_stall = (((self.FL*3.28)/0.5136)^(1/2))/1.994 #in m/s
-        wingLoadingMax = 0.5*rho*(V_stall^2)*self.Cl_max
+        V_stall = (((self.FL*3.28)/0.5136)^(1/2))/1.994 #in m/s.
+        wingLoadingMax = 0.5*rho*(V_stall**2)*self.Cl_max
         return wingLoadingMax
 
     #Using Raymer Method.
     def landingRaymer(self,Sa,KR):
-        ALD = self.FL*(3/5) # FAR25 requirement
+        ALD = self.FL*(3/5) # FAR25 requirement.
         wingLoadingMax = ((ALD-Sa)/(0.51*KR))*self.Cl_max
         return wingLoadingMax
 
     #Using Raymer Method.
     def stallConstraint(self,rho):
-        wingLoadingMax = 0.5*rho*(self.MaxVstall^2)*self.Cl_max
+        wingLoadingMax = 0.5*rho*(self.max_Vstall**2)*self.Cl_max
         return wingLoadingMax
+
+    #POINT PERFORMANCE CONSTRAINTS - using thrust matching equations.
+
+    def cruise(self,V_inf,alt):
+        None
+    
+    def climb(self,climb_rate):
+        None
+    
+    def loiter(self,V_inf,alt):
+        None
+    
+
+
+
+
+
+
+
+    
     
 
     
