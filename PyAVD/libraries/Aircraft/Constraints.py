63f9662f79617333a7ea72be411e21ef0cb3c51a
Gabisanth <gabisanth11@gmail.com>
2021-10-20 13:23:47 +0100

    landing and takeoff constraints updated

--- PyAVD/libraries/Aircraft/Constraints.py
+++ PyAVD/libraries/Aircraft/Constraints.py
@@ -11,13 +11,32 @@ class Constraints:
         self.LD_max = LD_max
         self.FL = FieldLength
         self.MaxVstall = MaxVstall
-        self.Cl_max = Cl_max
+        self.Cl_max = Cl_max # this Cl_max is the same as Cl max for landing.
         self.Cl_clean = Cl_clean
     
     # Using Roskam Method.
     def takeoff(self):
-        TOP = self.FL/37.5 #gives TOP in lbs^2/(ft^2 hp)
-        # convert TOP into kg^2/(m^2 N)
+        TOP = self.FL/37.5 #gives TOP in lbs/(ft^2)
+        TOP = (TOP/2.2046)*9.81*(3.28^2)# convert TOP into N/m^2
+        ClmaxTakeoff = self.Cl_clean + 0.7*(self.Cl_max - self.Cl_clean)
+
+    #Using Roskam Method.
+    def landingRoskam(self,rho):
+        V_stall = (((self.FL*3.28)/0.5136)^(1/2))/1.994 #in m/s
+        wingLoadingMax = 0.5*rho*(V_stall^2)*self.Cl_max
+        return wingLoadingMax
+
+    #Using Raymer Method.
+    def landingRaymer(self,Sa,KR):
+        ALD = self.FL*(3/5) # FAR25 requirement
+        wingLoadingMax = ((ALD-Sa)/(0.51*KR))*self.Cl_max
+        return wingLoadingMax
+    
+    
+    
+
+    
+
 
 
 
