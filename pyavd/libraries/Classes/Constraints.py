e91d9841f4eb9072e16d617ddc6c5d6bff1782cc
Gabisanth <gabisanth11@gmail.com>
2021-11-04 23:00:53 +0000

    Updated Climb functions. Need to be checked

--- pyavd/libraries/Classes/Constraints.py
+++ pyavd/libraries/Classes/Constraints.py
@@ -47,10 +47,10 @@ class Constraints(Config, Optimiser):
         ---> S_takeoff = 37.5 * TOP (FAR25)
         """
         TOP = constraint.FL / (37.5 * (u.ft ** 3 / u.lb))
-        ClmaxTakeoff = constraint.Cl_clean + 0.7 * (constraint.Cl_max - constraint.Cl_clean)
+        constraint.ClmaxTakeoff = constraint.Cl_clean + 0.7 * (constraint.Cl_max - constraint.Cl_clean)
 
         # Calculate Thrust/Weight ratio as a function of wing loading
-        constraint.TW_takeoff = constraint.WS / ((ClmaxTakeoff * 9.81 * TOP.to(u.kg / u.m**2)) / 1.21)
+        constraint.TW_takeoff = constraint.WS / ((constraint.ClmaxTakeoff * 9.81 * TOP.to(u.kg / u.m**2)) / 1.21)
         print(f"TW_TO:{constraint.TW_takeoff.to_base_units()}")
 
 
@@ -110,26 +110,47 @@ class Constraints(Config, Optimiser):
         return (alpha / beta) * (term1 + term3 + term4)
         
     
-    def climb(constraint, climb_gradient, V_inf, alpha): 
-        # Convert climb gradient (%) into climb rate (dh/dt)
-        climb_rate = (climb_gradient * V_inf.to(u.kts)).magnitude * u.ft / u.min
-        climb_rate = climb_rate.to(u.m / u.s)
+    def climb(constraint, climb_gradient, V_inf, dCd0, pe): # pe is the percentage factor change in Oswald efficiency factor.
+        # # Convert climb gradient (%) into climb rate (dh/dt)
+        # climb_rate = (climb_gradient * V_inf.to(u.kts)).magnitude * u.ft / u.min
+        # climb_rate = climb_rate.to(u.m / u.s)
 
-        # Cd0 and e affected by takeoff and landing flap settings during positive and negative climb.
-        constraint.Cd0_takeoff = constraint.Cd0 + 0.02
-        constraint.e_takeoff = constraint.e * 0.95
+        # # Cd0 and e affected by takeoff and landing flap settings during positive and negative climb.
+        # constraint.Cd0_takeoff = constraint.Cd0 + 0.02
+        # constraint.e_takeoff = constraint.e * 0.95
 
-        return constraint.thrustMatching(climb_rate, V_inf, alpha, 0 * u.m)
+        # return constraint.thrustMatching(climb_rate, V_inf, alpha, 0 * u.m)
+
+        constraint.Cd0_takeoff = constraint.Cd0 + dCd0
+        constraint.e_takeoff = constraint.e * pe
+        Cl = constraint.ClmaxTakeoff*(constraint.max_Vstall/V_inf)**2
+
+        LD = Cl/(constraint.Cd0_takeoff + ((Cl)**2)/(np.pi*Config.aspect_ratio*Config.e))
+        TW = (1/LD) + climb_gradient/100
+        return constraint.WS.magnitude *0 + TW
 
 
-    def go_around_climb(constraint, climb_gradient, V_inf, alpha):
-        # Convert climb gradient (%) into climb rate (dh/dt)
-        climb_rate = climb_gradient * V_inf.to(u.kts) #in ft/min
 
-        constraint.Cd0_goaround = constraint.Cd0 + 0.07
-        constraint.e_goaround = constraint.e * 0.9
+    def go_around_climb(constraint, climb_gradient, V_inf, dCd0, pe, alpha):
+        # # Convert climb gradient (%) into climb rate (dh/dt)
+        # climb_rate = climb_gradient * V_inf.to(u.kts) #in ft/min
+
+        # constraint.Cd0_goaround = constraint.Cd0 + 0.07
+        # constraint.e_goaround = constraint.e * 0.9
         
-        return constraint.thrustMatching(climb_rate, V_inf, alpha, 0 * u.m)
+        # return constraint.thrustMatching(climb_rate, V_inf, alpha, 0 * u.m)
+
+        constraint.Cd0_goaround = constraint.Cd0 + dCd0
+        constraint.e_goaround = constraint.e * pe
+        Cl = constraint.Cl_max*(constraint.max_Vstall/V_inf)**2
+
+        LD = Cl/(constraint.Cd0_takeoff + ((Cl)**2)/(np.pi*Config.aspect_ratio*Config.e))
+        TW = ((1/LD) + climb_gradient/100)*alpha
+
+        return constraint.WS.magnitude*0 + TW
+
+
+
 
 
     def designPoint(constraint):
@@ -152,12 +173,20 @@ class Constraints(Config, Optimiser):
         TW_absCeiling = constraint.thrustMatching(0 * u.m / u.s, mach_to_speed((45000 * u.ft).to(u.m).magnitude, 0.6), 0.94, 45000 * u.ft)
         TW_cruise2 = constraint.thrustMatching(0 * u.m / u.s, 200 * u.kts, 0.5, 26000 * u.ft)
         
-        # Climb
-        TW_climb1 = constraint.climb(0.1, 1.1 * constraint.V_stall, 0.98) * 2.0
-        TW_climb2 = constraint.climb(2.4, 1.1 * constraint.V_stall, 0.98) * 2.0
-        TW_climb3 = constraint.climb(1.2, 1.25 * constraint.V_stall, 0.98) * 2.0
-        TW_climb4 = constraint.climb(2.1, 1.5 * constraint.V_stall, 0.3) * 2.0
-        TW_climb5 = constraint.climb(3.2, 1.3 * constraint.V_stall, 0.3)
+        # # Climb
+        # TW_climb1 = constraint.climb(0.1, 1.1 * constraint.V_stall, 0.98) * 2.0
+        # TW_climb2 = constraint.climb(2.4, 1.1 * constraint.V_stall, 0.98) * 2.0
+        # TW_climb3 = constraint.climb(1.2, 1.25 * constraint.V_stall, 0.98) * 2.0
+        # TW_climb4 = constraint.climb(2.1, 1.5 * constraint.V_stall, 0.3) * 2.0
+        # TW_climb5 = constraint.climb(3.2, 1.3 * constraint.V_stall, 0.3)
+
+        ## New Climb
+        TW_climb1 = constraint.climb(0.1,1.1 * constraint.V_stall,0.04,0.95) * 2.0
+        TW_climb2 = constraint.climb(2.4, 1.1 * constraint.V_stall,0.02,0.95) * 2.0
+        TW_climb3 = constraint.climb(1.2, 1.25 * constraint.V_stall, 0, 1) * 2.0
+        TW_climb4 = constraint.go_around_climb(2.1, 1.5 * constraint.V_stall,0.05,0.9,0.3) * 2.0
+        TW_climb5 = constraint.go_around_climb(3.2, 1.3 * constraint.V_stall, 0.07,0.9,0.3)
+
         
         # Loiter
         TW_loiter = constraint.thrustMatching(0 * u.m / u.s, 150 * u.kts, 0.2, 5000 * u.ft)
@@ -182,11 +211,13 @@ class Constraints(Config, Optimiser):
         plt.plot(WS,constraint.TW_cruise_maxSpeed,'g',label='Cruise Max Speed', linewidth=3)
         plt.plot(WS,TW_absCeiling,'tab:pink',label='Absolute ceiling')
         plt.plot(WS,TW_loiter,'tab:cyan',label='Loiter')
+
         plt.plot(WS,TW_climb1,'y',label='Climb 1st Segment OEI')
         plt.plot(WS,TW_climb2,'m',label='Climb 2nd Segment OEI')
         plt.plot(WS,TW_climb3,'c',label='Climb 3rd Segment OEI')
         plt.plot(WS,TW_climb4,'tab:olive',label='Climb from approach OEI')
         plt.plot(WS,TW_climb5,'tab:brown',label='Climb from landing AEO')
+
 
         plt.plot(constraint.x_designPoint, constraint.y_designPoint,'r',marker = "o",label='Selected Design Point', markersize=10)
         plt.annotate("Design Point", (constraint.x_designPoint, constraint.y_designPoint),
