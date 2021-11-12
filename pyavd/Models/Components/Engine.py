022f20ab605057bd72dd7fd156d68a2f33c849d9
Raihaan Usman <raihaan.usman@gmail.com>
2021-11-12 17:25:53 +0000

    takeoff completed

--- pyavd/Models/Components/Engine.py
+++ pyavd/Models/Components/Engine.py
@@ -1,6 +1,6 @@
-from gpkit import Model, Variable, VectorVariable, Vectorize, parse_variables
+from gpkit import Model, Vectorize, VectorVariable, parse_variables, ureg as u
 from gpkit.constraints.tight import Tight
-from gpkit import ureg as u
+import numpy as np
 
 
 class Engine(Model):
