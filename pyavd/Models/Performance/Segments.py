61479bb48ff103e96a186ae5a78ee0036ed27ae0
Raihaan Usman <raihaan.usman@gmail.com>
2021-11-12 23:53:40 +0000

    Added aircraft() argument to segment classes
    Co-authored-by: Gabisanth11 <Gabisanth11@users.noreply.github.com>

--- pyavd/Models/Performance/Segments.py
+++ pyavd/Models/Performance/Segments.py
@@ -8,20 +8,26 @@ class Takeoff(Model):
 
     Variables
     ---------
-    TW                      [-]           Thrust to Weight
     TOP                     [kg/m^2]      Takeoff Parameter
-    WS                      [N/m^2]       Wing Loading
     FL              1200    [m]           Field Length
     CL_max_TO               [-]           Maximum Lift Coefficient
     g               9.81    [m/s^2]       Gravitational Acceleration
     
     """
     @parse_variables(__doc__, globals())
-    def setup(self, CL_max=2.1, CL_clean=1.5):
+    def setup(self, aircraft=None, CL_max=2.1, CL_clean=1.5):
+        # Importing Aircraft() parameters - TODO: remove temporary try/except
+        try:
+            TW = aircraft.TW
+            WS = aircraft.WS
+        except AttributeError:
+            TW = Variable("TW",     "",         "Thrust to Weight ratio")
+            WS = Variable("WS",     "N/m^2",    "Wing Loading")
         
+        # Constraints dictionary
         constraints = {}
 
-        # Equations are handled as constraints! REPLACE WITH COMMENT ON CONSTRAINT
+        # Equations are also handled as constraints! REPLACE WITH COMMENT ON CONSTRAINT
         k1 = Variable('k', 37.5, 'ft^3/lb', 'Some Random Constant')
         constraints.update({"Takeoff Parameter" : [
                     TOP == FL / k1                                                    ]})
@@ -32,21 +38,60 @@ class Takeoff(Model):
 
         # Annnnnnd here
         constraints.update({"Thrust to Weight constraint" : [
-                    TW >= WS / ((CL_max * g * TOP) / 1.21)                            ]})
+                    TW >= WS / ((CL_max_TO * g * TOP) / 1.21)                         ]}) 
         
         # Returning all constraints
         return constraints
+
+
+class Climb(Model):
+    """Climb regime model
+
+    Variables
+    ---------
+    CL_max_TO               [-]           Maximum Lift Coefficient
+    Cd0                     [-]           Zero Lift Drag Coefficient
+    e                       [-]           Oswald Efficiency
+    LD                      [-]           Lift-Drag Ratio at climb stage
+
+    """
+    @parse_variables(__doc__, globals())
+    def setup(self, aircraft, dCd0, de, climb_gradient, AR, CL_max=2.1, CL_clean=1.5):
+        
+        constraints = {}
+
+        # Equations are handled as constraints! REPLACE WITH COMMENT ON CONSTRAINT
+        constraints.update({"Takeoff Cd0" : [
+                    Cd0_TO == Cd0 + dCd0                                                    ]})
+
+        constraints.update({"Takeoff Cd0" :[
+                    e_TO == e + de                                                   ]})
+
+        
+        constraints.update({"Lift Coeffcient @ Takeoff Equation" : [
+                    CL_max_TO == CL_clean + 0.7 * (CL_max - CL_clean)                 ]})
+        
+        # Aaand here
+        constraints.update({"Lift-Drag Ratio at Climb stage" : [
+                    LD == Cl_max_TO/(Cd0_TO + ((Cl_max_TO)**2)/(np.pi*AR*e_TO))                 ]})
+
+        # Annnnnnd here
+        constraints.update({"Thrust to Weight constraint" : [
+                    TW >= (1/LD) + climb_gradient/100                            ]})
+        
+        # Returning all constraints
+        return constraints
+
         
 
 # Implement following models
-# Climb
 # Cruise
 # Descent
 # Loiter
 # Landing
 
 
-# General Segment model - couples aircraft model with flight segment and state
+# General Segment model - couples flight segment with the aircraft model and state
 class Segment(Model):
     def setup(self, Aircraft):
         constraints = []
