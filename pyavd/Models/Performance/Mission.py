328f3ccd01fd18cb2fe1e284c43daa2083cd1d9f
Raihaan Usman <raihaan.usman@gmail.com>
2021-11-17 01:07:06 +0000

    started adding to Mission()

--- pyavd/Models/Performance/Mission.py
+++ pyavd/Models/Performance/Mission.py
@@ -12,12 +12,11 @@ class Mission(Model):
     @parse_variables(__doc__, globals())
     def setup(self, aircraft=None):
         self.aircraft = aircraft
-        mission = self.mission = []
-
-        # with Vectorize(4):  # four flight segments
-        #     self.fs = Segment(aircraft)
+        mission = self.mission = {}
         
-        mission += Takeoff(aircraft)
-        mission += Climb(aircraft)
+        # TODO: link to Streamlit frontend - session_state.mission_profile
+        mission.update({"Takeoff": Takeoff(aircraft)})
+        mission.update({"Climb": Climb(0.04, 0.05, 0.1, aircraft=aircraft, goAround=False)})
 
-        return {"flight segment": mission}
+
+        return mission
